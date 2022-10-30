daaf6a1abbebf33bbdd71121feaad52f64dc9c42
areedy <a.reedy@gmail.com>
2022-10-29 21:02:46 -0400

    cleaning up script.js file

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,7 @@ var currentTime = moment();
 var currentTimeHour = Number(currentTime.format("H"));
 var timeBoxes = $(".form-control");
 console.log(timeBoxes.length);
+
 //color coding time blocks based on past present and future
 $(".form-control").each(function (index) {
     var timeBoxTime = $(this).data("time");
@@ -45,71 +46,6 @@ function loadSavedTasks () {
     
 }
 
+//run function to display saved entries on page load
 loadSavedTasks();
-
-//var = $('#'variable)
-/* 
-good
-function getTaskSubmit () {
-    $(".form-control").each(function (index) {
-        var timeBoxTime = $(this).data("time");
-        console.log(timeBoxTime);
-        console.log($('#'+timeBoxTime).val());
-    })
-}
- */
-/* $("button").on('click', function (event) {
-    //var timeBoxInput = $('input[name="task"]').val();
-    console.log($(event.target).data("time"));
-    //console.log(timeBoxInput);
-})  */
-    
-
-//var timeBoxInput = $('input[name="task"]').val();
-
-//get input
-/* function getTaskSubmit(event) {
-    event.preventDefault();
-
-    var timeBoxInput = $(this,'input:text').val();
-    console.log(timeBoxInput);
-    if (!timeBoxInput) {
-        console.log("No task entered");
-        return;
-    }
-    console.log(timeBoxInput);
-    var taskHour = $(this).data("time"); 
-    if (taskHour == 9) {
-        localStorage.setItem("hour9", timeBoxInput);
-    } else if (taskHour == 10) {
-        localStorage.setItem("hour10", timeBoxInput);
-    } else if (taskHour == 11) {
-        localStorage.setItem("hour11", timeBoxInput);
-    } else if (taskHour == 12) {
-        localStorage.setItem("hour12", timeBoxInput);
-    } else if (taskHour == 13) {
-        localStorage.setItem("hour13", timeBoxInput);
-    } else if (taskHour == 14) {
-        localStorage.setItem("hour14", timeBoxInput);
-    } else if (taskHour == 15) {
-        localStorage.setItem("hour15", timeBoxInput);
-    } else if (taskHour == 16) {
-        localStorage.setItem("hour16", timeBoxInput);
-    } else if (taskHour == 17) {
-        localStorage.setItem("hour17", timeBoxInput);
-    } else {
-        console.log("Error");
-    }
-    
-}
-
- */
-//save function
-
-
-//to do
-
-
-//save value from boxes in local storage
-
 
